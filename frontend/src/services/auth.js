26c9f1573cee4a77e27dd07aaab0937bdbcb96db
ishwari03 <pusadkarishwari@gmail.com>
2026-07-25 21:34:57 +0530

    fix: update auth.js to use production URL

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/auth",
+  baseURL: "https://devlens-ai-backend-i0tg.onrender.com/api/auth",
 });
 
 // Register User
